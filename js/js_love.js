0a6e8b3b6a2aaf0ca2668a7cfe609808b029d815
AyedhSh <alshmary_ad@yahoo.com>
2017-03-03 13:47:58 +0300

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,6 +1,14 @@
 // Functions here
 function showJsLove() {
-};
+  $.get('https://api.github.com/repos/rails/rails/commits',function(){
+   console.log("I before Love");
+  });
+
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+ 
+  console.log("Love before JavaScript");
+})
+}
 
 $(document).ready(function (){
   showJsLove();
