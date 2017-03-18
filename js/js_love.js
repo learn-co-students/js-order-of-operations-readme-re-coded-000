369920629187b0b4a45d479b194fc10220f7529b
Muataz Aziz <muataz.aziz91@yahoo.com>
2017-03-18 12:18:11 +0000

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,21 @@
-// Functions here
-function showJsLove() {
-};
-
 $(document).ready(function (){
   showJsLove();
 });
+
+// Functions here
+function showJsLove() {
+  console.log("I");
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function() {
+    console.log("JavaScript");
+  });
+  console.log("Love");
+};
+
+// //another way
+// function showJsLove() {
+// 	console.log('I')
+// 	$.getJSON('https://api.github.com/repos/rails/rails/commits',function(data){
+// 		console.log('Love');
+// 		console.log('JavaScript');
+// 	})
+// };
